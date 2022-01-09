43e429917f77eab8ef982c4e9dca3fe9c1c04d64
Vartika Gupta <vartika.gupta13@gmail.com>
2022-01-08 21:20:26 -0500

    Added Form component

--- stories/index.js
+++ stories/index.js
@@ -17,7 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
-
+import Form from "components/Appointment/Form"
 
 //BUTTON
 storiesOf("Button", module)
@@ -166,4 +166,16 @@ storiesOf("Appointment", module)
   .add("Error", () => <Error
     message="Could not delete appointment."
     onClose={action("onClose")}
-    />)
+  />)
+  .add("Form edit", () => <Form
+    name="Jane Doe"
+    interviewers={interviewers}
+    interviewer={2}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
+  />)
+  .add("Form create", () => <Form
+    interviewers={interviewers}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
+  />)
