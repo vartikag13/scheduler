e07eaf1699cfbc3bfaf82d658b47c7b52f0a7227
Vartika Gupta <vartika.gupta13@gmail.com>
2022-01-28 00:24:51 -0500

    Added cypress tests

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,62 +4,56 @@ import DayList from "./DayList";
 import Appointment from "./Appointment";
 import "components/Application.scss";
 import useApplicationData from "hooks/useApplicationData";
-import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
-
+import {
+	getAppointmentsForDay,
+	getInterview,
+	getInterviewersForDay,
+} from "helpers/selectors";
 
 export default function Application(props) {
-  const {
-    state,
-    setDay,
-    bookInterview,
-    cancelInterview,
-  } = useApplicationData();
+	const { state, setDay, bookInterview, cancelInterview } =
+		useApplicationData();
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
-  const dailyInterviewers = getInterviewersForDay(state, state.day);
+	const dailyAppointments = getAppointmentsForDay(state, state.day);
+	const dailyInterviewers = getInterviewersForDay(state, state.day);
 
-  const schedule = dailyAppointments.map((apt) => {
-    const interview = getInterview(state, apt.interview);
-    return (
-      <Appointment
-      key={apt.id}
-      id={apt.id}
-      time={apt.time}
-      interview={interview}
-      interviewers={dailyInterviewers}
-      bookInterview={bookInterview}
-      cancelInterview={cancelInterview}  
-    />
-    ) 
-  })
-  
+	const schedule = dailyAppointments.map((apt) => {
+		const interview = getInterview(state, apt.interview);
+		return (
+			<Appointment
+				key={apt.id}
+				id={apt.id}
+				time={apt.time}
+				interview={interview}
+				interviewers={dailyInterviewers}
+				bookInterview={bookInterview}
+				cancelInterview={cancelInterview}
+			/>
+		);
+	});
 
-  return (
-    <main className="layout">
-      <section className="sidebar">
-        <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-  <DayList
-    days={state.days}
-    day={state.day}
-    setDay={setDay}
-  />
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
-      </section>
-      <section className="schedule">
-        {schedule}
-        <Appointment key="last" time="5pm" />
-      </section>
-    </main>
-  );
+	return (
+		<main className="layout">
+			<section className="sidebar">
+				<img
+					className="sidebar--centered"
+					src="images/logo.png"
+					alt="Interview Scheduler"
+				/>
+				<hr className="sidebar__separator sidebar--centered" />
+				<nav className="sidebar__menu">
+					<DayList days={state.days} day={state.day} setDay={setDay} />
+				</nav>
+				<img
+					className="sidebar__lhl sidebar--centered"
+					src="images/lhl.png"
+					alt="Lighthouse Labs"
+				/>
+			</section>
+			<section className="schedule">
+				{schedule}
+				<Appointment key="last" time="5pm" />
+			</section>
+		</main>
+	);
 }
